edc7de55d0359e42c1dc193ac6dd9ced346498e1
Naser Al-Asbahi <nasserqahtan0@gmail.com>
2025-07-31 19:46:10 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* header */}
+      {/* sidebar */}
+      {/* maincontent */}
+    </>
+  );
+}
+
+export default App;
